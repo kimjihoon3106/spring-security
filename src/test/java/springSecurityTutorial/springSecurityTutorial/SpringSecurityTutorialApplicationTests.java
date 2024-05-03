c5e6491c2ddf0dfb9e3d53911beea4e01491ed31
kimjihoon3106 <kimjihoon3106@gmail.com>
2024-05-03 23:32:00 +0900

    start: springSecurityTutorial

---
+++ src/test/java/springSecurityTutorial/springSecurityTutorial/SpringSecurityTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package springSecurityTutorial.springSecurityTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
